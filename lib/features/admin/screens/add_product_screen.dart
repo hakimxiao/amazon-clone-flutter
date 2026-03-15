5cfb12c36fadc35c6c5268e39f8833ac462468a8
hakimxiao <alhakimabusolihin@gmail.com>
2026-03-15 13:53:52 +0700

    (feat) Melakukkan setup fungsionalitas image upload pada client
    # package
    - menginstall package tambahan yaitu [file_picker, cloudinary_public] yang akan melakukkan handler pic image dan juga upload image ke provider cloudianary
    
    # Hal baru
    - membuat utilitas class baru pada utils.dart yaitu pickImages(){} yang bertugas untuk melakukkan handler pengambilan gambar pada sistem devices kemudian disimpan ke program
    -* membuat sebuah service baru yaitu admin_services.dart pada features/admin *Ini adalah hendler penting yang melakukkan komunikasi dengan API sendiri maupun provider
    
    # penyempurnaan / implmentasi
    - pada add_products_screen.dart sekarang sudah mengimplementasikan logic dari image upload serta memanfaatkan condisional rendering untuk penampilan gambar atau dotted border
    
    #models
    - Membuat model baru yaitu product untuk membudahkan aktivitas fromJson dan toJson

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:amazon_clone_flutter/common/widgets/custom_button.dart';
 import 'package:amazon_clone_flutter/common/widgets/custom_textfield.dart';
 import 'package:amazon_clone_flutter/constants/global_variables.dart';
+import 'package:amazon_clone_flutter/constants/utils.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -21,6 +25,8 @@ class _AddProductScreenState extends State<AddProductScreen> {
 
   String category = 'Mobiles';
 
+  List<File> images = [];
+
   List<String> productCategories = [
     'Mobiles',
     'Essentials',
@@ -28,6 +34,14 @@ class _AddProductScreenState extends State<AddProductScreen> {
     'Books',
     'Fashion',
   ];
+
+  void selectImages() async {
+    var result = await pickImages();
+
+    setState(() {
+      images = result;
+    });
+  }
 
   @override
   void dispose() {
@@ -60,35 +74,51 @@ class _AddProductScreenState extends State<AddProductScreen> {
             child: Column(
               children: [
                 SizedBox(height: 20),
-                DottedBorder(
-                  options: RoundedRectDottedBorderOptions(
-                    dashPattern: [10, 4],
-                    strokeWidth: 1.5,
-                    radius: Radius.circular(10),
-                    strokeCap: StrokeCap.round,
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(Icons.folder_open, size: 40),
-                        const SizedBox(height: 15),
-                        Text(
-                          'Select product images',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.grey.shade400,
+                images.isNotEmpty
+                    ? CarouselSlider(
+                        items: images.map((i) {
+                          return Builder(
+                            builder: (BuildContext context) =>
+                                Image.file(i, fit: BoxFit.cover, height: 200),
+                          );
+                        }).toList(),
+                        options: CarouselOptions(
+                          viewportFraction: 1,
+                          height: 200,
+                        ),
+                      )
+                    : GestureDetector(
+                        onTap: selectImages,
+                        child: DottedBorder(
+                          options: RoundedRectDottedBorderOptions(
+                            dashPattern: [10, 4],
+                            strokeWidth: 1.5,
+                            radius: Radius.circular(10),
+                            strokeCap: StrokeCap.round,
+                          ),
+                          child: Container(
+                            width: double.infinity,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.folder_open, size: 40),
+                                const SizedBox(height: 15),
+                                Text(
+                                  'Select product images',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey.shade400,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
+                      ),
                 SizedBox(height: 30),
                 CustomTextfield(
                   controller: productNameController,
---
+++ lib/models/product.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Product {
+  final String name;
+  final String description;
+  final double price;
+  final double quantity;
+  final String category;
+  final List<String> images;
+  final String? id;
+  final String? userId;
+  Product({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.quantity,
+    required this.category,
+    required this.images,
+    this.id,
+    this.userId,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'description': description,
+      'price': price,
+      'quantity': quantity,
+      'category': category,
+      'images': images,
+      '_id': id,
+      'userId': userId,
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      name: map['name'] as String,
+      description: map['description'] as String,
+      price: map['price'] as double,
+      quantity: map['quantity'] as double,
+      category: map['category'] as String,
+      images: List<String>.from((map['images'] as List<String>)),
+      id: map['_id'] != null ? map['_id'] as String : null,
+      userId: map['userId'] != null ? map['userId'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Product.fromJson(String source) =>
+      Product.fromMap(json.decode(source) as Map<String, dynamic>);
+}
